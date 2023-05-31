c3b928ba7a5def919c00b645c6e7de2d4def692c
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2023-05-31 11:12:24 +0300

    Fix ROS-version on folder name

--- turtlebot3_bringup/tbenv.sh
+++ turtlebot3_bringup/tbenv.sh
@@ -3,7 +3,7 @@
 export ROS_MASTER_URI=http://192.168.0.102:11311
 export ROS_IP=192.168.0.137
 
-source /opt/ros/melodic/setup.bash
+source /opt/ros/noetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
 
 exec "$@"
