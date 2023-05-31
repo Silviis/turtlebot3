96623f5523a48f659cb456703db6dcab14c000e9
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2023-05-31 13:01:18 +0300

    Orange init commit

--- turtlebot3_bringup/tbenv.sh
+++ turtlebot3_bringup/tbenv.sh
@@ -2,6 +2,7 @@
 
 export ROS_MASTER_URI=http://192.168.0.102:11311
 export ROS_IP=192.168.0.223
+export LDS_MODEL=LDS-01
 
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
